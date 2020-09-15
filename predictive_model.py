4b9a961fba94619a2a01e3b82fd98f87ab733792
Pedro Elias <epedro520@gmail.com>
2020-09-15 09:46:23 -0300

    upgrade:atributes created

--- predictive_model.py
+++ predictive_model.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import numpy as np 
-import mathplot_lib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
-titanic_data =pd.read_csv("test.csv")
-titanic_data_train = pd.read_csv("train.csv")
-
-print(titanic_data_train)
-print(titanic_data)
-
-
+titanic_dataset_train = pd.DataFrame(pd.read_csv("train.csv"))
+titanic_dataset_test = pd.DataFrame(pd.read_csv("test.csv"))
+    
+titannic_train = (titanic_dataset_train[["Survived"]]>1)
+titanic_train_Pclass = (titanic_dataset_test[["Sex"]] == "Male")
 
 
+from sklearn.linear_model import SGDClassifier
+sgd_clf = SGDClassifier(random_state=42)
 
