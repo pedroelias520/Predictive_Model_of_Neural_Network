52606b9e89ca71de5ba2436f6931a37d7ac598e8
Pedro Elias <epedro520@gmail.com>
2020-09-15 12:08:56 -0300

    upgrade: classificator added to code

--- predictive_model.py
+++ predictive_model.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 titanic_dataset_train = pd.DataFrame(pd.read_csv("train.csv"))
 titanic_dataset_test = pd.DataFrame(pd.read_csv("test.csv"))
     
-titannic_train = (titanic_dataset_train[["Survived"]]>1)
-titanic_train_Pclass = (titanic_dataset_test[["Sex"]] == "Male")
+titanic_train = (titanic_dataset_train[["Survived"]]>1)
+titanic_train_Pclass = (titanic_dataset_train[["Sex"]])
 
 
 from sklearn.linear_model import SGDClassifier
 sgd_clf = SGDClassifier(random_state=42)
-
+sgd_clf.fit(titanic_train, titanic_train_Pclass)
