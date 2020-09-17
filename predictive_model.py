5fb4a62f16c4bef320545342b3b9ff3f2c477b63
Pedro Elias <epedro520@gmail.com>
2020-09-16 22:32:21 -0300

    upgrade: confusion matrix created

--- predictive_model.py
+++ predictive_model.py
@@ -5,11 +5,32 @@ import matplotlib.pyplot as plt
 
 titanic_dataset_train = pd.DataFrame(pd.read_csv("train.csv"))
 titanic_dataset_test = pd.DataFrame(pd.read_csv("test.csv"))
-    
-titanic_train = (titanic_dataset_train[["Survived"]]>1)
-titanic_train_Pclass = (titanic_dataset_train[["Sex"]])
+   
 
+#treinamento de classificadores 
+#Onde o segmento de treino achará os passageiros sobreviventes
+titanic_dataset_train_Column = titanic_dataset_train[["Survived"]]
+titanic_train_segment = (titanic_dataset_train[["Survived"]]==1)
 
 from sklearn.linear_model import SGDClassifier
 sgd_clf = SGDClassifier(random_state=42)
-sgd_clf.fit(titanic_train, titanic_train_Pclass)
+sgd_clf.fit(titanic_dataset_train_Column, titanic_train_segment)
+some_person = titanic_dataset_train_Column.iloc[[1]]
+
+#teste de classificador
+sgd_clf.predict(some_person)
+
+#validação cruzada 
+from sklearn.model_selection import cross_val_score
+cross_val_score(sgd_clf, titanic_dataset_train_Column, titanic_train_segment, cv=3, scoring="accuracy")
+
+#matriz de consfusão
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import confusion_matrix 
+
+y_train_pred = cross_val_predict(sgd_clf, titanic_dataset_train_Column, titanic_train_segment, cv=3)
+confusion_matrix(titanic_train_segment,y_train_pred)
+
+#controle de precisão
+from sklearn.metrics import precision_score, recall_score
+precision_score(titanic_train_segment, y_train_pred)
