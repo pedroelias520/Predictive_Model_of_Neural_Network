8059ebb59e2a682fce5ee4186b2da04be19415b3
Pedro Elias <epedro520@gmail.com>
2020-09-17 22:55:02 -0300

    upgrade: matrix converted to array

--- predictive_model.py
+++ predictive_model.py
@@ -11,10 +11,11 @@ titanic_dataset_test = pd.DataFrame(pd.read_csv("test.csv"))
 #Onde o segmento de treino achará os passageiros sobreviventes
 titanic_dataset_train_Column = titanic_dataset_train[["Survived"]]
 titanic_train_segment = (titanic_dataset_train[["Survived"]]==1)
+segment = np.array(titanic_train_segment).ravel()
 
 from sklearn.linear_model import SGDClassifier
 sgd_clf = SGDClassifier(random_state=42)
-sgd_clf.fit(titanic_dataset_train_Column, titanic_train_segment)
+sgd_clf.fit(titanic_dataset_train_Column, segment)
 some_person = titanic_dataset_train_Column.iloc[[1]]
 
 #teste de classificador
@@ -22,15 +23,15 @@ sgd_clf.predict(some_person)
 
 #validação cruzada 
 from sklearn.model_selection import cross_val_score
-cross_val_score(sgd_clf, titanic_dataset_train_Column, titanic_train_segment, cv=3, scoring="accuracy")
+cross_val_score(sgd_clf, titanic_dataset_train_Column, segment, cv=3, scoring="accuracy")
 
 #matriz de consfusão
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import confusion_matrix 
 
-y_train_pred = cross_val_predict(sgd_clf, titanic_dataset_train_Column, titanic_train_segment, cv=3)
-confusion_matrix(titanic_train_segment,y_train_pred)
+y_train_pred = cross_val_predict(sgd_clf, titanic_dataset_train_Column,segment, cv=3)
+confusion_matrix(segment,y_train_pred)
 
 #controle de precisão
 from sklearn.metrics import precision_score, recall_score
-precision_score(titanic_train_segment, y_train_pred)
+precision_score(segment, y_train_pred)
